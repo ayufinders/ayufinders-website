aed0dfb761fc0e183065e619a719b658889ec93f
Manik Sharma <maniksharma.rke@gmail.com>
2025-05-18 18:35:46 +0530

    added

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://ayufinders.com', // ← replace with your domain
-  generateRobotsTxt: true,
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 5000,
-};
